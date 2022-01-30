5c45e10bc58d3eaa1f8a1645036eba1df55ae728
Andrey Vladykov <avladykov@outlook.com>
2022-01-30 18:57:41 +0300

    Read settings from .env file.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import os
+
+import constants
 from key_generator import KeyGenerator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ALPHABET = os.getenv(constants.ALPHABET)
+KEY_LENGTH = int(os.getenv(constants.KEY_LENGTH))
 
 if __name__ == '__main__':
-    alphabet = [chr(char) for char in range(ord('a'), ord('z') + 1)]
-    key_generator = KeyGenerator(alphabet=alphabet, key_length=5)
+    key_generator = KeyGenerator(alphabet=ALPHABET, key_length=KEY_LENGTH)
     key_generator.generate()
     print(key_generator.keys[0])
